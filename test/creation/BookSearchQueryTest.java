92e94db9686c9e26a7c94fb25ac46394339d2e16
Robert Chatley <robert@develogical.com>
2021-12-18 12:34:52 +0000

    Use a pom.xml to organise structure

--- test/creation/BookSearchQueryTest.java
+++ test/creation/BookSearchQueryTest.java
@@ -1,7 +1,7 @@
 package creation;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
